edfdb525f456ae9b32a96d3c766c5d959499eb9a
ooheunda <ooheunda@gmail.com>
2025-05-08 02:20:07 +0900

    chore: ConfigModule 설정 및 .env.sample 파일 추가

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 import { AppModule } from './app.module';
 
 async function bootstrap(): Promise<void> {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  const config = app.get(ConfigService);
+  const port = config.get<number>('PORT') || 3000;
+
+  await app.listen(port);
 }
 bootstrap();
