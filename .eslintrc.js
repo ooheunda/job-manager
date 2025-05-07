8b0852c7decd6a9f68d54fb3c7a36098cf4fbce7
ooheunda <ooheunda@gmail.com>
2025-05-08 02:18:05 +0900

    chore: import, jest 플러그인을 포함하는 eslint 설정

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,10 +5,12 @@ module.exports = {
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint/eslint-plugin', 'import', 'jest'],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
+    'plugin:import/recommended',
+    'plugin:jest/recommended',
   ],
   root: true,
   env: {
@@ -17,9 +19,69 @@ module.exports = {
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
     '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/naming-convention': [
+      'error',
+      { selector: 'variableLike', format: ['camelCase', 'UPPER_CASE'] },
+      { selector: 'property', format: ['camelCase'] },
+      { selector: 'class', format: ['PascalCase'] },
+      { selector: 'classMethod', format: ['camelCase'] },
+      { selector: 'objectLiteralMethod', format: ['camelCase'] },
+      {
+        // object literal proptery 에 다양한 케이스 허용
+        // 사용 예: env, open api, DB Key
+        selector: 'objectLiteralProperty',
+        format: ['camelCase', 'UPPER_CASE', 'snake_case'],
+      },
+    ],
+    '@typescript-eslint/explicit-function-return-type': ['warn', { allowExpressions: true }], // 함수의 반환 타입 명시
+    '@typescript-eslint/no-explicit-any': 'error', // any 사용시 경고
+    'import/order': [
+      // import 순서 정렬
+      'warn',
+      {
+        alphabetize: {
+          order: 'asc', // 알파벳 순서로 정렬
+        },
+        'newlines-between': 'always', // 그룹 별 newline 으로 구분
+        // 기본 ["builtin", "external", "parent", "sibling", "index"] 순서로, 변경 가능
+      },
+    ],
+  },
+  overrides: [
+    {
+      files: ['**/*.constants.ts'],
+      rules: {
+        '@typescript-eslint/naming-convention': [
+          'error',
+          {
+            selector: 'variable',
+            format: ['camelCase', 'UPPER_CASE'],
+          },
+        ],
+      },
+    },
+    {
+      files: ['**/*.decorator.ts'],
+      rules: {
+        '@typescript-eslint/naming-convention': [
+          'error',
+          {
+            selector: 'variable',
+            format: ['camelCase', 'PascalCase'],
+          },
+        ],
+      },
+    },
+  ],
+  settings: {
+    'import/resolver': {
+      node: {
+        moduleDirectory: ['node_modules', 'src'],
+        extensions: ['.js', '.ts'],
+      },
+    },
   },
 };
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,6 +1,7 @@
-import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
+import { Test, TestingModule } from '@nestjs/testing';
+import request from 'supertest';
+
 import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
@@ -15,10 +16,8 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  it('/ (GET)', async () => {
+    const res = await request(app.getHttpServer()).get('/');
+    expect(res.status).toBe(404);
   });
 });
