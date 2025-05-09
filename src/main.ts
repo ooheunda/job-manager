099c3346cd268b7fca2ba935cba0722bbf2e6b03
ooheunda <ooheunda@gmail.com>
2025-05-09 17:55:54 +0900

    chore: Swagger 모듈 추가 및 document 생성

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 
@@ -11,7 +12,17 @@ async function bootstrap(): Promise<void> {
   const config = app.get(ConfigService);
   const port = config.get<number>('PORT') || 3000;
 
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Job Manager')
+    .setDescription('작업 관리 시스템 API 문서')
+    .setVersion('1.0')
+    .addTag('jobs')
+    .addServer(`http://localhost:${port}/api`)
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api-docs', app, document);
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   app.setGlobalPrefix('api');
 
   await app.listen(port);
